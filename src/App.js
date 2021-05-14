e7dbde5d5e2a091eaa54f4215651e473c957ca26
7204-OMER <Eyupbhr24@gmail.com>
2021-05-15 00:33:59 +0300

    onClick continue

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Card from "./components/Card";
+import UserCard from "./components/Card";
 
 function App() {
   return (
     <div className="App">
-      <Card />
+      <UserCard />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -8,19 +8,33 @@ import man from "../assets/man.svg";
 import map from "../assets/map.svg";
 import padlock from "../assets/padlock.svg";
 import phone from "../assets/phone.svg";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function Card() {
+const UserCard = () => {
+  const [userList, setUserList] = useState();
+
+  const userData = () => {
+    axios.get("https://randomuser.me/api/").then((res) => {
+      setUserList(res.data.results);
+    });
+  };
+  useEffect(() => {
+    userData();
+  }, []);
+
   return (
     <div>
       <img src={cw} alt="cw" />
-      <div className="logo">
-        <img src={man} alt="man" />
-        <img src={mail} alt="mail" />
-        <img src={growingman} alt="growing" />
-        <img src={map} alt="map" />
-        <img src={phone} alt="phone" />
-        <img src={padlock} alt="padlock" />
-        <button>NEW USER</button>
+      <div className="logo" key={index}>
+        <p>info</p>
+        <img src={man} alt="man" className="logo" />
+        <img src={mail} alt="mail" className="logo" />
+        <img src={growingman} alt="growing" className="logo" />
+        <img src={map} alt="map" className="logo" />
+        <img src={phone} alt="phone" className="logo" />
+        <img src={padlock} alt="padlock" className="logo" />
+        <button onClick={UserData}>NEW USER</button>
         <button>ADD USER</button>
       </div>
       <div>
@@ -28,6 +42,6 @@ function Card() {
       </div>
     </div>
   );
-}
+};
 
-export default Card;
+export default UserCard;
